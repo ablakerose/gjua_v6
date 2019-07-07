5b796de6641e16fba5241f7be44ee8e091f7d999
Blake Rose <bdecktor@gmail.com>
2019-07-07 00:31:51 +0000

    added in spec.homepage as requested

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,21 +1,21 @@
 
-html = open("https://www.womenslinkworldwide.org/en/awards/cases")
-doc = Nokogiri::HTML(html)
+# html = open("https://www.womenslinkworldwide.org/en/awards/cases")
+# doc = Nokogiri::HTML(html)
 
-class Scraper
+# class Scraper
  
-  def get_page
-      doc = Nokogiri::HTML(open("https://www.womenslinkworldwide.org/en/awards/cases"))
+#   def get_page
+#       doc = Nokogiri::HTML(open("https://www.womenslinkworldwide.org/en/awards/cases"))
  
-  end
+#   end
  
-end
+# end
 
-Scraper.new.get_page
+# Scraper.new.get_page
 
 
-# cases = doc.css("#instructors .team-holder .person-box")
+# # cases = doc.css("#instructors .team-holder .person-box")
 
-# # instructors.each do |instructor| 
-# #   puts "Flatiron School <3 " + instructor.css("h2").text
-# # end
+# # # instructors.each do |instructor| 
+# # #   puts "Flatiron School <3 " + instructor.css("h2").text
+# # # end
