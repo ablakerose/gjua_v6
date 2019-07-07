208c8bacccbddc921525776b2098928b62e583c0
Blake Rose <bdecktor@gmail.com>
2019-07-07 01:44:22 +0000

    began scraping case title method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,15 @@ class Scraper
  
   def get_page
       doc = Nokogiri::HTML(open("https://www.womenslinkworldwide.org/en/awards/cases"))
-  binding.pry
+      binding.pry
+  end
+  
+  def case_title
+    case_title = doc.css(".toggleArt").text.split(" , ")
+    
+    # ["\"Boys will be boys\"Delivered on concreteDecriminalization of AbortionAccess to Assisted ReproductionToo Old for SexRespect women’s rightsLiving without abuserCase of Ángela González - State ResponsibilityFight Against Sexual HarassmentPension with a Gender PerspectiveEviction from the Family HomeNo more revictimizationMurder of trans activist Diana SacayánRestrictive abortion law violates human rightsTwo-finger test\"La Manada\" caseCriminalizing Pregnant WomenRaped, pregnant, and jailedJuana is in my houseCommunity Service for Sexual AbuseTrans people denied equal rightsProhibited to mention abortionDeath Sentence for rape victimDeliberately deceptive informationAssaulted in a hotelConviction for unfaithful women\"At least she won't get pregnant\"Guilty of ending a \"harmonious marriage\"Stolen Babies“Feelings were running high”"]
+    
+    
   end
  
 end
